03466b2c20559236bf167099b8364673e27e3721
Fimber <fimber6@gmail.com>
2023-02-24 11:52:18 +0100

    Added search functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -3,28 +3,30 @@ import { Box,  Typography} from '@mui/material';
 import { useParams } from 'react-router-dom';
 
 import { fetchFromAPI } from '../utils/fetchFromAPI';
-import { Videos} from './';
+import { Videos } from './';
 
 
 const SearchFeed = () => {
 
   
-  const [videos, setVideos] = useState([])
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=`)
-    .then((data) => setVideos(data.items) )
-  }, []);
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+     .then((data) => setVideos(data.items) )
+  }, [searchTerm]);
 
 
   return (
     <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: 2 }}>
         <Typography variant='h4' fontWeight="bold" mb={2} ml={2} sx={{color: 'white'}}>
-          Results for <span style={ {color:'#36454F'}}>videos</span>
+          Results for: <span style={ {color:'#36454F'}}>{searchTerm}</span> videos
         </Typography>
         <Videos videos={videos}/>
-      </Box>
-  )
-}
+    </Box>
+  );
+};
 
-export default SearchFeed
+export default SearchFeed;
